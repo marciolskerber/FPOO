e380b4a890263ee3fa92e9e215fc8cee75e86f3e
marciolskerber <marciolskerber@gmail.com>
2025-05-28 11:57:29 -0300

    Projeto Maven tic-tac-toe criado

--- tic-tac-toe/src/test/java/appTest/AppBackgroundTest.java
+++ tic-tac-toe/src/test/java/appTest/AppBackgroundTest.java
@@ -4,16 +4,28 @@ import java.net.URL;
 
 import javax.swing.ImageIcon;
 
+import ticTacToe.gui.MainWindow;
+import ticTacToe.gui.Table;
+
 public class AppBackgroundTest {
-	
+
+	static ImageIcon loadImage(String pathWithFileName) {
+		URL url = AppBackgroundTest.class.getResource(pathWithFileName);
+		return new ImageIcon(url);
+	}
+
 	public static void main(String[] args) {
 		final String path = "../ticTacToe/images/";
-		
+
 		MainWindow window = new MainWindow();
-		URL url = AppBackgroundTest.class.getResource(path +"background.jpg");
-		
-		ImageIcon icon = new ImageIcon(url);
+
+		ImageIcon icon = loadImage(path + "background.jpg");
 		window.setBackground(icon);
+
+		icon = loadImage(path + "tic-tac-toe.png");
+		Table table = new Table(50, 50, 200, 200, icon);
+		window.add(table);
+
 	}
 
 }
